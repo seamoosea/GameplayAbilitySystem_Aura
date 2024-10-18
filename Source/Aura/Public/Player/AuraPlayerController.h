b5df2343350641f50d0f1b4e78e2b759cc9eec5c
seamoosea <69767053+seamoosea@users.noreply.github.com>
2024-10-17 19:33:57 -0700

    Enhanced Input and Aura player controller

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,32 @@
+// Copyright Seamoose Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+public:
+    AAuraPlayerController();
+
+protected:
+    virtual void BeginPlay() override;
+
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Input")
+    TObjectPtr<UInputMappingContext> AuraContext;
+
+};
